6c52dbb742ba5c1b6f5ba81a867daaece6a00a29
chanchal goyal <miss71910@gmail.com>
2024-08-29 18:51:31 +0530

    create components and adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is iNotebook</h1>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
